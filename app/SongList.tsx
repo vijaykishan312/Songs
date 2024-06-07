97c006ace853f012802f1ca36613f7ed693bd465
vijay <vijaykishan.vyas@dotsquares.com>
2024-06-07 19:02:22 +0530

    update

--- app/SongList.tsx
+++ app/SongList.tsx
@@ -9,9 +9,9 @@ import { router, useLocalSearchParams } from 'expo-router';
 
 const SongList = () => {
 
-  const item:any = useLocalSearchParams();
-  console.log(" This is item -=-=-=-=-=-=-=-=- "+JSON.stringify(item));
-  
+  const item: any = useLocalSearchParams();
+  console.log(" This is item -=-=-=-=-=-=-=-=- " + JSON.stringify(item));
+
 
   return (
     <ImageBackground resizeMode='cover' style={{ flex: 1, paddingHorizontal: ScreenRatio(1) }} source={Images.wallpaper1_image}>
@@ -22,17 +22,17 @@ const SongList = () => {
             <Image source={Images.icon_back} style={{ height: ScreenRatio(2.2), width: ScreenRatio(2.2), resizeMode: "contain", tintColor: Colors.gray005 }} />
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => { router.back() }} style={{ height: ScreenRatio(35), width: ScreenRatio(35), borderRadius: ScreenRatio(50), backgroundColor: Colors.gray20, shadowOffset: { height: 0, width: 0 }, shadowOpacity: 1, shadowRadius: 7, elevation: 3, shadowColor: Colors.orange10, alignItems: "center", justifyContent: "center", alignSelf:"center", marginTop:ScreenRatio(5) }}>
-            <Image source={item?.image} style={{ height: ScreenRatio(34), width: ScreenRatio(34), resizeMode: "cover", borderRadius:ScreenRatio(50)}} />
+          <TouchableOpacity onPress={() => { router.back() }} style={{ height: ScreenRatio(35), width: ScreenRatio(35), borderRadius: ScreenRatio(50), backgroundColor: Colors.gray20, shadowOffset: { height: 0, width: 0 }, shadowOpacity: 1, shadowRadius: 7, elevation: 3, shadowColor: Colors.orange10, alignItems: "center", justifyContent: "center", alignSelf: "center", marginTop: ScreenRatio(5) }}>
+            <Image source={item?.image} style={{ height: ScreenRatio(34), width: ScreenRatio(34), resizeMode: "cover", borderRadius: ScreenRatio(50) }} />
           </TouchableOpacity>
 
-          <View>
-            <Text style={{}}>
-
+          <View style={{ marginTop: ScreenRatio(3) }}>
+            <Text style={{ fontSize: ScreenRatio(3), color: Colors.orange8, fontWeight: "700", textAlign: "center" }}>
+              {item?.songName}
             </Text>
-          <TouchableOpacity onPress={() => { router.back() }} style={{ height: ScreenRatio(5), width: ScreenRatio(5), borderRadius: ScreenRatio(15), backgroundColor: Colors.gray20, shadowOffset: { height: 0, width: 0 }, shadowOpacity: 1, shadowRadius: 7, elevation: 3, shadowColor: Colors.orange10, alignItems: "center", justifyContent: "center" }}>
-            <Image source={Images.icon_back} style={{ height: ScreenRatio(2.2), width: ScreenRatio(2.2), resizeMode: "contain", tintColor: Colors.gray005 }} />
-          </TouchableOpacity>
+            <TouchableOpacity onPress={() => { router.back() }} style={{ height: ScreenRatio(5), width: ScreenRatio(5), borderRadius: ScreenRatio(15), backgroundColor: Colors.gray20, shadowOffset: { height: 0, width: 0 }, shadowOpacity: 1, shadowRadius: 7, elevation: 3, shadowColor: Colors.orange10, alignItems: "center", justifyContent: "center" }}>
+              <Image source={Images.icon_downloads} style={{ height: ScreenRatio(2.2), width: ScreenRatio(2.2), resizeMode: "contain", tintColor: Colors.gray005 }} />
+            </TouchableOpacity>
           </View>
         </View>
       </SafeAreaView>
--- assets/Images.tsx
+++ assets/Images.tsx
@@ -4,7 +4,8 @@ const Images = {
     song1_image: require('./Images/song1.jpeg'),
     song2_image: require('./Images/song2.jpeg'),
     wallpaper1_image: require('./Images/wallpaper1.jpeg'),
-    icon_back: require('./Images/icon_back.png')
+    icon_back: require('./Images/icon_back.png'),
+    icon_downloads: require('./Images/icon_downloads.png'),
 }
 
 export default Images
